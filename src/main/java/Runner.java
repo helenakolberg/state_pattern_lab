37e448517d4f373c6b5601ce3e956b22fbb654ee
Helena <inger.helena@gmail.com>
2020-09-03 15:24:58 +0100

    modified Runner demo

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,16 @@
 public class Runner {
     public static void main(String[] args) {
+        Kid kid1 = new Kid(1);
+        System.out.println(kid1.eat());
+        System.out.println(kid1.play());
+
+        kid1.setAge(2);
+        System.out.println(kid1.eat());
+        System.out.println(kid1.play());
+
+        kid1.setAge(3);
+        System.out.println(kid1.eat());
+        System.out.println(kid1.play());
 
     }
 
